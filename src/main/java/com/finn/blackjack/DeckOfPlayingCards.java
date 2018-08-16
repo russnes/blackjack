ea1c037841874231393eb42488e0bac399a52625
Simen <simen.s.russnes@gmail.com>
2018-08-16 23:04:12 +0200

    removed unused imports

--- src/main/java/com/finn/blackjack/DeckOfPlayingCards.java
+++ src/main/java/com/finn/blackjack/DeckOfPlayingCards.java
@@ -1,7 +1,5 @@
 package com.finn.blackjack;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
